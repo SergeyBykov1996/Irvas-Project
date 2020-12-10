cc2e36e13c51d3d6eb1b57c040812162aefcc693
Sergey <bikova58@mail.ru>
2020-12-10 23:53:50 +0300

    tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 import './slider';
 import modal from './modules/modal';
+import tabs from './modules/tabs';
 
 
 window.addEventListener('DOMContentLoaded', () => {
     modal();
+    tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
 });
 
 console.log(1);
